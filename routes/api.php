1596393653d478d6ec45a3ff34207fd55646b689
Franck Kianguebeni (AGL) <franck.kianguebeni@congo-terminal.com>
2026-06-20 15:02:43 +0100

    first commit

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/health', function () {
+    return response()->json([
+        'status' => 'franck ok',
+        'app' => config('app.name'),
+    ]);
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
